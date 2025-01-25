a148269b25cca59cf79393379f0da61a5cec1d1b
antick-arch <e241003@ugrad.iiuc.ac.bd>
2025-01-25 19:22:22 +0600

    Update index.html, script.js, and style.css for new page structure and animations

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     menuButton.addEventListener("click", () => {
         alert("Menu button clicked!");
     });
+
+    const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, libero.";
+    const textContainer = document.querySelector(".page2 .animated-text");
+    text.split("").forEach((char, index) => {
+        const span = document.createElement("span");
+        span.textContent = char;
+        textContainer.appendChild(span);
+    });
 });
